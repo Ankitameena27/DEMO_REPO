41d1472d42d991e4a97b03d6451d43c69ab3ee94
Ankitameena27 <130924317+Ankitameena27@users.noreply.github.com>
2023-09-16 16:21:34 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,16 +1,17 @@
-pipeline  {
-      agent any
+pipeline {
+    agent any
 
-      environment {
-      CREDENTIALS = credentials('demo_user_password')
-      }
+    environment {
+        CREDENTIALS = credentials('demo_user_password')
+    }
 
-      stages {
-         stage('demo')
-             steps {
-                  echo "${CREDENTIALS_USR}"
-                  echo "${CREDENTIALS_PSW}"
-                  }
-                }
-              }
-            }  
+    stages {
+        stage('demo') {
+            steps {
+                echo "${CREDENTIALS_USR}"
+                echo "${CREDENTIALS_PSW}"
+            }
+        }
+    }
+}
+ 
