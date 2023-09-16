255e24f362b6d53d3c8dcb4b298bd0a49b68698e
Ankitameena27 <130924317+Ankitameena27@users.noreply.github.com>
2023-09-16 16:48:19 +0530

    Create jenkinsfile.sh

---
+++ jenkinsfile.sh
@@ -0,0 +1,18 @@
+pipeline {
+    agent any
+
+    environment {
+        CREDENTIALS = credentials('demo_user_password')
+    }
+
+    stages {
+        stage('demo') {
+            steps {
+                script {
+                    // Add the shebang line for the shell script
+                    sh '#!/bin/bash\n./print_user_name.sh'
+                }
+            }
+        }
+    }
+}
