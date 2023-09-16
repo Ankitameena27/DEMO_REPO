dee1f896e6d8786977d8b5111ae27a91d84a6d3f
Ankitameena27 <130924317+Ankitameena27@users.noreply.github.com>
2023-09-16 15:50:35 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,16 @@
+pipeline  {
+      agent any
+
+      environment {
+      CREDENTIALS = credentials('demo_user_password')
+      }
+
+      stages {
+         stage('demo')
+             steps {
+                  echo "${CREDENTIALS_USR}"
+                  echo "${CREDENTIALS_PSW}"
+                  }
+                }
+              }
+            }  
